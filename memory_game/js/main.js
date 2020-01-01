9b886dae521c6bbcc2bbb3e341df43bcd4b0ac14
Yuna <Yunah503@gmail.com>
2019-12-31 23:14:36 -0500

    created game logic functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,23 @@ alert("Hello, friends.");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2) {
-	console.log("There are " + cardsInPlay.length + " cards in play.")
-	if(cardOne === cardTwo) {
-		alert("You found a match!");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+	  console.log("Sorry, try again.");
+	}
+}
+var flipCard = function(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
 	}
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
+
